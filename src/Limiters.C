ae0f120efa499664e6e6983526acf94f88cd66f7
Benyamin Krisna <benkris@DNa1cc674.SUNet>
2025-06-06 00:14:58 -0700

    test

--- src/Limiters.C
+++ src/Limiters.C
@@ -26,25 +26,41 @@ namespace nalu{
   template<typename T>
   T minmod_limiter(const T& dq, const T& dm, const T& small) {
     // Minmod limiter
-    const T r = dq / (dm + small);
-    return T(2.0) * stk::math::max(T(0.0), stk::math::min(T(1.0), r));
+    const T prod = dm * dq;
+    const T minAbs = stk::math::min(stk::math::abs(dm), stk::math::abs(dq));
+    const T limiter = stk::math::if_else(prod > 0.0, minAbs, T(0.0));
+    return limiter / (stk::math::abs(dm) + small);
   }
 
   template<typename T>
   T superbee_limiter(const T& dq, const T& dm, const T& small) {
     // superbee_limiter
-    const T r = dq / (dm + small);
-    const T a = stk::math::min(2.0 * r, T(1.0));
-    const T b = stk::math::min(r, T(2.0));
-    return T(2.0) * stk::math::max(T(0.0), stk::math::max(a, b));
+    const T prod = dm * dq;
+    const T two_dq = 2.0 * stk::math::abs(dq);
+    const T two_dm = 2.0 * stk::math::abs(dm);
+
+    const T min1 = stk::math::min(two_dq, stk::math::abs(dm));
+    const T min2 = stk::math::min(stk::math::abs(dq), two_dm);
+    const T maxMin = stk::math::max(min1, min2);
+
+    const T limiter = stk::math::if_else(prod > 0.0, maxMin, T(0.0));
+    return limiter / (stk::math::abs(dm) + small);
   }
 
   template<typename T>
   T ultrabee_limiter(const T& dq, const T& dm, const T& small) {
     // ultrabee_limiter
-    const T r = dq / (dm + small);
-    const T limit = stk::math::min(T(2.0), stk::math::min(2.0 * r, 1.0 + r));
-    return T(2.0) * stk::math::if_then_else(r > 0.0, limit, T(0.0));
+    const T prod = dm * dq;
+    const T abs_dm = stk::math::abs(dm);
+    const T abs_dq = stk::math::abs(dq);
+
+    const T two_dm = 2.0 * abs_dm;
+    const T two_dq = 2.0 * abs_dq;
+    const T one_plus = abs_dm + abs_dq;
+
+    const T minAll = stk::math::min(stk::math::min(two_dm, two_dq), one_plus);
+    const T limiter = stk::math::if_else(prod > 0.0, minAll, T(0.0));
+    return limiter / (abs_dm + small);
   }
 
 template double van_leer_limiter<double>(const double&, const double&, const double&);
